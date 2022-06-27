a9d7f33f6ed065ed690d644583ff8dd0d82d3c4f
your Name <mohamedabdulayubm@gmail.com>
2022-06-27 21:51:45 +0530

    [Ayub] Add CreateContact

--- Day9_Address/Program.cs
+++ Day9_Address/Program.cs
@@ -1,14 +1,15 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using AddressBook_OOPS;
+using Day9_Address;
 
 
-Console.WriteLine("Welcome to Address Book Program");
+
+    Console.WriteLine("Welcome to Address Book Program");
 
 
-AddressBook address = new AddressBook();
-address.createContact();
-
+    AddressBook address = new AddressBook();
+    address.CreateContact();
